9fd36e094e864597ba47811a5ad47ad38ebabe27
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2021-10-25 14:44:43 +0100

    bau: Allow es6 sytanx in linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   env: {
     node: true,
+    es6: true,
   },
   root: true,
-  extends: [
-    "eslint:recommended",
-    "prettier"
-  ],
+  extends: ["eslint:recommended", "prettier"],
   rules: {
     "no-console": 2,
     "padding-line-between-statements": [

c26137b445086e63ffd57b9f2f352c309cc57b57
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2021-11-17 18:00:11 +0000

    feat: Use cfenv for api url

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
 
+const cfenv = require("cfenv");
+const appEnv = cfenv.getAppEnv();
+const serviceConfig = {};
+
+if (!appEnv.isLocal) {
+  serviceConfig.coreBackAPIUrl = appEnv.getServiceURL("core-back-api");
+}
+
 module.exports = {
-  API_BASE_URL: process.env.API_BASE_URL,
+  API_BASE_URL: serviceConfig.coreBackAPIUrl || process.env.API_BASE_URL,
   AUTH_PATH: "/authorize",
   PORT: process.env.PORT || 3000,
   SESSION_SECRET: process.env.SESSION_SECRET,

bb0230797f28e72575274ddb95c38275586b65cd
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2021-11-25 17:07:21 +0000

    PYI-367 Add tests for empty credential issuer base url
    Co-authored-by: amrit.sidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
    Co-authored-by: Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>

--- src/app/credential-issuer/middleware.test.js
+++ src/app/credential-issuer/middleware.test.js
@@ -1,16 +1,46 @@
 const proxyquire = require("proxyquire");
-const configStub = { CREDENTIAL_ISSUER_BASE_URL: "http://example.com" }
-const redirectToAuthorize = proxyquire("./middleware", {
-  "../../lib/config": configStub,
-});
-describe('credential issuer middleware', () => {
+
+describe("credential issuer middleware", () => {
   describe("redirectToAuthorize", () => {
+    let req;
+    let res;
+    let configStub;
+
+    beforeEach(() => {
+      req = {};
+      res = { redirect: sinon.fake(), send: sinon.fake() };
+      configStub = {};
+    });
     it("should successfully be redirected", async function () {
-      let req;
-      let res = { redirect: sinon.fake() };
+      configStub.CREDENTIAL_ISSUER_BASE_URL = "http://example.com";
+      const redirectToAuthorize = proxyquire("./middleware", {
+        "../../lib/config": configStub,
+      });
+
       await redirectToAuthorize(req, res);
 
-      expect(res.redirect).to.have.been.calledWith(`http://example.com/authorize`)
+      expect(res.redirect).to.have.been.calledWith(
+        `http://example.com/authorize`
+      );
+    });
+
+    context("with a missing base url", () => {
+      it("should send 500 error", async () => {
+        const redirectToAuthorize = proxyquire("./middleware", {
+          "../../lib/config": configStub,
+        });
+        await redirectToAuthorize(req, res);
+
+        expect(res.send).to.have.been.calledWith(500);
+      });
+      it("should not call redirect", async () => {
+        const redirectToAuthorize = proxyquire("./middleware", {
+          "../../lib/config": configStub,
+        });
+        await redirectToAuthorize(req, res);
+
+        expect(res.redirect).not.to.have.been.called;
+      });
     });
   });
 });

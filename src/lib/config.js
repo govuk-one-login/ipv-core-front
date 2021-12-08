5b0a25741d96ef1a2b4ed0c5a412f4468003f48d
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2021-12-08 08:09:08 +0000

    BAU: Extract BASE_URL from the environment
    We're using the BASE_URL env variable to construct a redirect URL to
    send to the credential issuer, but never actually extracting it from the
    environment. This resulted in redirect URL's with `undefined` as the
    host.

--- src/lib/config.js
+++ src/lib/config.js
@@ -12,6 +12,7 @@ module.exports = {
   API_BASE_URL: serviceConfig.coreBackAPIUrl || process.env.API_BASE_URL,
   AUTH_PATH: "/authorize",
   API_REQUEST_EVIDENCE_PATH: "/request-evidence",
+  BASE_URL: process.env.BASE_URL,
   CREDENTIAL_ISSUER_BASE_URL: process.env.CREDENTIAL_ISSUER_BASE_URL,
   CREDENTIAL_ISSUER_AUTH_PATH: "/authorize",
   CREDENTIAL_ISSUER_ID: "PassportIssuer",

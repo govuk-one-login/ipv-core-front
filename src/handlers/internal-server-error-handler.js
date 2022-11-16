35cf6a6d4cb7cd576b33d4204c831b29425dec8a
LaiTang-GDS <lai.tang@digital.cabinet-office.gov.uk>
2022-11-16 17:42:59 +0000

    BAU: Added try catch when setting status code to avoid exception rendered to the user when status property is an int and not a function

--- src/handlers/internal-server-error-handler.js
+++ src/handlers/internal-server-error-handler.js
@@ -1,17 +1,18 @@
 const { HTTP_STATUS_CODES } = require("../app.constants");
+const { logError } = require("../app/shared/loggerHelper");
 
 module.exports = {
   serverErrorHandler(err, req, res, next) {
-    if (req.log) {
-      req.log.info({
-        message: {
-          label: "internal-server-error-handler - extra error details",
-          error: err,
-          req,
-          res,
-        },
-      });
-    }
+    // if (req.log) {
+    //   req.log.info({
+    //     message: {
+    //       label: "internal-server-error-handler - extra error details",
+    //       error: err,
+    //       req,
+    //       res,
+    //     },
+    //   });
+    // }
 
     if (res.headersSent) {
       return next(err);
@@ -21,7 +22,13 @@ module.exports = {
       return res.render("errors/session-ended.njk");
     }
     res.err = err; // this is required so that the pino logger does not log new error with a different stack trace
-    res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR);
+
+    try {
+      res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR);
+    } catch (e) {
+      logError(req, e, "Bad response - status is not a function");
+    }
+
     res.render("ipv/pyi-technical-unrecoverable.njk");
   },
 };

9d98ade6018195762f17c5b5efc2dad3254aeb07
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2023-03-23 13:51:32 +0000

    adding RUM snippet and mechanism for switching appID in environment

--- src/assets/javascript/application.js
+++ src/assets/javascript/application.js
@@ -245,3 +245,28 @@ window.GOVSignIn.Cookies = cookies;
   w.disableAfterClick = disableAfterClick;
 
 })(window);
+
+import { AwsRum } from 'aws-rum-web';
+
+try {
+  const config = {
+    sessionSampleRate: 0.1,
+    endpoint: "https://dataplane.rum.eu-west-2.amazonaws.com",
+    telemetries: ["errors","performance","http"],
+    allowCookies: false,
+    enableXRay: false
+  };
+
+  const APPLICATION_ID = process.env.RUM_APPLICATION_ID ;
+  const APPLICATION_VERSION = '1.0.0';
+  const APPLICATION_REGION = 'eu-west-2';
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}

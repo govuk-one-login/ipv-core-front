655957e6b4378963556a0b6555062e042d5b81c1
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2023-05-04 10:26:27 +0100

    BAU: Fix codesmell

--- src/app/oauth2/middleware.js
+++ src/app/oauth2/middleware.js
@@ -63,7 +63,7 @@ module.exports = {
   validateFeatureSet: async (req, res, next) => {
     try {
       const featureSet = req.query.featureSet;
-      const isValidFeatureSet = /^[a-zA-Z0-9_]{1,32}$/.test(featureSet);
+      const isValidFeatureSet = /^\w{1,32}$/.test(featureSet);
       if (!isValidFeatureSet) {
         throw new Error("Invalid feature set ID");
       }

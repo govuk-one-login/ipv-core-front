3fed6e00d331c84fdb9ead41b2f1f3594a88f78e
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2023-05-19 10:03:26 +0100

    PYIC-2809: Add featureset page
    Move featureset to IPV middleware

--- src/app/oauth2/middleware.js
+++ src/app/oauth2/middleware.js
@@ -59,18 +59,4 @@ module.exports = {
 
     next();
   },
-
-  validateFeatureSet: async (req, res, next) => {
-    try {
-      const featureSet = req.query.featureSet;
-      const isValidFeatureSet = /^\w{1,32}$/.test(featureSet);
-      if (!isValidFeatureSet) {
-        throw new Error("Invalid feature set ID");
-      }
-      req.session.featureSet = featureSet;
-      next();
-    } catch (error) {
-      return next(error);
-    }
-  },
 };

216aff9443ce94ddf65562922d7c6693268fde9c
Derren Wilson <derren.wilson@digital.cabinet-office.gov.uk>
2023-07-18 17:08:57 +0100

    PYIC-2873 Show date of birth on ipv-reuse in GDS standard format

---
+++ src/app/shared/debugJourneyHelper.js
@@ -0,0 +1,32 @@
+module.exports = {
+  samplePersistedUserDetails: {
+    data: {
+      name: "Alessandro Cholmondeley-Featherstonehaugh",
+      dateOfBirth: "1984-02-29",
+      addresses: [
+        {
+          subBuildingName: "Flat 24",
+          buildingName: "Wollatorn House",
+          buildingNumber: "7",
+          streetName: "Batchelor Street",
+          addressLocality: "London",
+          postalCode: "N15 0EY",
+        },
+        {
+          subBuildingName: "Flat 18",
+          buildingName: "The Paper Apartments",
+          buildingNumber: "162",
+          streetName: "Offord Road",
+          addressLocality: "London",
+          postalCode: "N2 1NS",
+        },
+        {
+          buildingNumber: "7",
+          streetName: "Acorne Terrace",
+          addressLocality: "London",
+          postalCode: "N16 4QF",
+        },
+      ],
+    },
+  },
+};

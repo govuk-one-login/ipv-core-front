03a37b217feba0f531f9e0ee18f1b85dfa6fc5cc
Derren Wilson <derren.wilson@digital.cabinet-office.gov.uk>
2023-07-19 11:06:22 +0100

    PYIC-2873 update debug data to use all fields

--- src/app/shared/debugJourneyHelper.js
+++ src/app/shared/debugJourneyHelper.js
@@ -4,6 +4,24 @@ module.exports = {
       name: "Alessandro Cholmondeley-Featherstonehaugh",
       dateOfBirth: "1984-02-29",
       addresses: [
+        // all possible address parts
+        // for explanations of each part
+        // https://alliescomputing.com/knowledge-base/glossary-of-address-terms
+        // https://docs.ideal-postcodes.co.uk/docs/data/paf
+        {
+          departmentName: "Room 25",
+          organisationName: "Turing House",
+          subBuildingName: "Block 4b",
+          buildingName: "Vital Living",
+          buildingNumber: "1",
+          dependentStreetName: "Circular Square",
+          streetName: "7 O'Reilly Way",
+          doubleDependentAddressLocality: "Oxford Lane",
+          dependentAddressLocality: "University Quarter",
+          addressLocality: "Lancaster",
+          postalCode: "M12 7LU",
+        },
+        // example addresses
         {
           subBuildingName: "Flat 24",
           buildingName: "Wollatorn House",

23848c8b6b6056afa6a37bd5e3f5eae4985e04e7
Shivanshu Patel <shivanshu.patel@digital.cabinet-office.gov.uk>
2023-07-28 11:10:59 +0100

    PYIC-3288: add welsh langauge check for inset texts

--- src/app/ipv/router.js
+++ src/app/ipv/router.js
@@ -16,10 +16,20 @@ const {
 const csrfProtection = csrf({});
 const parseForm = bodyParser.urlencoded({ extended: false });
 
+
+function checkLanguage(req, res, next) {
+  const lang = req.cookies.lng;
+
+  // Set the flag "isWelsh" to true if the language is Welsh, otherwise set to false
+  res.locals.isWelsh = lang === "cy";
+
+  next();
+}
+
 router.get("/usefeatureset", validateFeatureSet, renderFeatureSetPage);
 
 router.get("/page/attempt-recovery", csrfProtection, renderAttemptRecoveryPage);
-router.get("/page/:pageId", csrfProtection, handleJourneyPage);
+router.get("/page/:pageId", csrfProtection, checkLanguage, handleJourneyPage);
 router.post(
   "/page/page-multiple-doc-check",
   parseForm,

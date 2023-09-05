ed13711b0790db3dc09ee409a0539a0768512f0d
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2023-09-05 13:10:23 +0100

    Added x-forwarded-for header As fallback option for adding client IP to session, REST API gateway adds x-forwarded-for header instead of 'forwarded' header

--- src/app/shared/ipAddressHelper.js
+++ src/app/shared/ipAddressHelper.js
@@ -3,7 +3,6 @@ const IPV4_MATCH = "\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b";
 
 module.exports = {
   getIpAddress: (req) => {
-
     if (req.headers && req.headers["forwarded"]) {
       const ipAddress = req.headers["forwarded"].match(IPV4_MATCH);
       if (ipAddress) {
@@ -11,7 +10,7 @@ module.exports = {
       } else {
         return unknown;
       }
-    }else if(req.headers && req.headers["x-forwarded-for"]) {
+    } else if (req.headers && req.headers["x-forwarded-for"]) {
       const ipAddress = req.headers["x-forwarded-for"].match(IPV4_MATCH);
       if (ipAddress) {
         return ipAddress[0];

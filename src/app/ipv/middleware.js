2170f96de7b7d5f80bff8121a5faf6da635b0cd0
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-02-08 15:38:19 +0000

    PYIC-3966: Revert let to const

--- src/app/ipv/middleware.js
+++ src/app/ipv/middleware.js
@@ -176,7 +176,7 @@ module.exports = {
   },
   handleJourneyPage: async (req, res, next) => {
     try {
-      let { pageId } = req.params;
+      const { pageId } = req.params;
       const { context } = req?.session || "";
 
       // Remove this as part of PYIC-4278

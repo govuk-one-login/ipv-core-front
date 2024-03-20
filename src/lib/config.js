fe12eab9114a3e3d9b721b33e1cb0122385bc760
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-03-20 14:08:21 +0000

    PYIC-4821: Use old app store links for now

--- src/lib/config.js
+++ src/lib/config.js
@@ -18,9 +18,9 @@ module.exports = {
   API_SESSION_INITIALISE: "/session/initialise",
   API_BUILD_PROVEN_USER_IDENTITY_DETAILS: "/user/proven-identity-details",
   APP_STORE_URL_ANDROID:
-    process.env.APP_STORE_URL_ANDROID || "https://play.google.com/store/apps",
+    process.env.APP_STORE_URL_ANDROID || "https://play.google.com/store/apps/details?id=uk.gov.documentchecking",
   APP_STORE_URL_APPLE:
-    process.env.APP_STORE_URL_APPLE || "https://www.apple.com/uk/app-store/",
+    process.env.APP_STORE_URL_APPLE || "https://apps.apple.com/gb/app/gov-uk-id-check/id1629050566",
   ENABLE_PREVIEW: process.env.ENABLE_PREVIEW === "development",
   EXTERNAL_WEBSITE_HOST: process.env.EXTERNAL_WEBSITE_HOST,
   PORT: process.env.PORT || 3000,

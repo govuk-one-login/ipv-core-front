b32c9c002cc4a93a198d289073396b83a27149bb
Roger V <roger.vanburgh@digital.cabinet-office.gov.uk>
2024-05-08 13:07:11 +0100

    PYIC-5829: Added missed commit

--- src/constants/update-details-journeys.js
+++ src/constants/update-details-journeys.js
@@ -3,7 +3,7 @@ module.exports = Object.freeze({
   UPDATE_NAMES_DOB: "names-dob",
   UPDATE_GIVEN_NAMES: "given-names-only",
   UPDATE_FAMILY_NAME: "family-name-only",
-  UPDATE_GIVEN_NAME_ADDRESS: "given-names-and-address",
-  UPDATE_FAMILY_NAMES_ADDRESS: "family-name-and-address",
+  UPDATE_GIVEN_NAMES_ADDRESS: "given-names-and-address",
+  UPDATE_FAMILY_NAME_ADDRESS: "family-name-and-address",
   UPDATE_CANCEL: "cancel",
 });

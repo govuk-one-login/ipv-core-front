fbf8833d0d4623dae45b0224c77313fe1d640b53
SophiaDWokoma <148252375+SophiaDWokoma@users.noreply.github.com>
2024-05-15 16:49:04 +0100

    add personal data headers to all api calls and unit tests

--- src/services/coreBackService.test.js
+++ src/services/coreBackService.test.js
@@ -1,14 +1,17 @@
 const sinon = require("sinon");
 const { expect } = require("chai");
 const proxyquire = require("proxyquire");
+const {
+  createPersonalDataHeaders,
+} = require("@govuk-one-login/frontend-passthrough-headers");
 
 const configStub = {
   API_CRI_CALLBACK: "/cri/callback",
   API_BASE_URL: "https://example.net",
   API_BUILD_PROVEN_USER_IDENTITY_DETAILS: "/proven-identity",
   API_SESSION_INITIALISE: "/session-initialise",
+  API_JOURNEY_EVENT: "/journey",
 };
-
 describe("CoreBackService", () => {
   let axiosInstanceStub = {};
   let CoreBackService;
@@ -38,6 +41,10 @@ describe("CoreBackService", () => {
   it("should postJourneyEvent with correct parameters and headers", async () => {
     // Arrange
     const event = "test_event";
+    req.headers = {
+      "txma-audit-encoded": "dummy-txma-header",
+      "x-forwarded-for": "198.51.100.10:46532",
+    };
 
     // Act
     await CoreBackService.postJourneyEvent(req, event);
@@ -54,6 +61,10 @@ describe("CoreBackService", () => {
           "feature-set": "test_feature_set",
           "ipv-session-id": "test_ipv_session_id",
           "client-session-id": "test_client_session_id",
+          ...createPersonalDataHeaders(
+            `${configStub.API_BASE_URL}${configStub.API_JOURNEY_EVENT}`,
+            req,
+          ),
         },
         logger: undefined,
       },
@@ -63,6 +74,10 @@ describe("CoreBackService", () => {
   it("should postSessionInitialise with correct parameters and headers", async () => {
     // Arrange
     const authParams = { someAuthParam: "someValue" };
+    req.headers = {
+      "txma-audit-encoded": "dummy-txma-header",
+      "x-forwarded-for": "198.51.100.10:46532",
+    };
 
     // Act
     await CoreBackService.postSessionInitialise(req, authParams);
@@ -79,6 +94,10 @@ describe("CoreBackService", () => {
           "feature-set": "test_feature_set",
           "ipv-session-id": "test_ipv_session_id",
           "client-session-id": "test_client_session_id",
+          ...createPersonalDataHeaders(
+            `${configStub.API_BASE_URL}${configStub.API_SESSION_INITIALISE}`,
+            req,
+          ),
         },
         logger: undefined,
       },
@@ -89,6 +108,10 @@ describe("CoreBackService", () => {
     // Arrange
     const body = { test_param: "someValue" };
     const errorDetails = { error_param: "anotherValue" };
+    req.headers = {
+      "txma-audit-encoded": "dummy-txma-header",
+      "x-forwarded-for": "198.51.100.10:46532",
+    };
 
     // Act
     await CoreBackService.postCriCallback(req, body, errorDetails);
@@ -105,6 +128,10 @@ describe("CoreBackService", () => {
           "feature-set": "test_feature_set",
           "ipv-session-id": "test_ipv_session_id",
           "client-session-id": "test_client_session_id",
+          ...createPersonalDataHeaders(
+            `${configStub.API_BASE_URL}${configStub.API_CRI_CALLBACK}`,
+            req,
+          ),
         },
         logger: undefined,
       },
@@ -115,10 +142,13 @@ describe("CoreBackService", () => {
     // Arrange
     const body = { test_param: "someValue" };
     const errorDetails = { error_param: "anotherValue" };
+    req.headers = {
+      "txma-audit-encoded": "dummy-txma-header",
+      "x-forwarded-for": "198.51.100.10:46532",
+    };
 
     // Act
     await CoreBackService.getProvenIdentityUserDetails(req, body, errorDetails);
-
     // Assert
     expect(axiosInstanceStub.get).to.have.been.calledWith("/proven-identity", {
       headers: {
@@ -128,6 +158,10 @@ describe("CoreBackService", () => {
         "feature-set": "test_feature_set",
         "ipv-session-id": "test_ipv_session_id",
         "client-session-id": "test_client_session_id",
+        ...createPersonalDataHeaders(
+          `${configStub.API_BASE_URL}${configStub.API_BUILD_PROVEN_USER_IDENTITY_DETAILS}`,
+          req,
+        ),
       },
       logger: undefined,
     });

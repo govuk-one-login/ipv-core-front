6189d7e7b64b5d677219a57a4fa698d5764361a6
Shivanshu Patel <shivanshu.patel@digital.cabinet-office.gov.uk>
2024-08-07 09:44:29 +0100

    PYIC-7042: add docker setup to run UI tests

--- test/browser/playwright.config.js
+++ test/browser/playwright.config.js
@@ -1,8 +1,18 @@
-const { defineConfig } = require("@playwright/test");
+const { defineConfig, devices } = require("@playwright/test");
 require("dotenv").config();
 
 export default defineConfig({
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        headless: true,
+      },
+    },
+  ],
   use: {
-    baseURL: process.env.WEBSITE_HOST,
-  }
+    baseURL: process.env?.WEBSITE_HOST || "http://localhost:4501",
+    headless: true,
+  },
 });

a27a24f2dcc5c0af25b83cb782f6c03af4c4058e
Shivanshu Patel <shivanshu.patel@digital.cabinet-office.gov.uk>
2024-08-14 10:11:26 +0100

    PYIC-7042: update submit button selector to match latest changes

--- test/browser/journey.spec.js
+++ test/browser/journey.spec.js
@@ -3,14 +3,14 @@ const { test, expect } = require("@playwright/test");
 test("Journey using mock server", async ({ page }) => {
   await page.goto("/oauth2/authorize?request=fake&client_id=orchestrator");
   await page.click("input[type='radio'][value='end']");
-  await page.click("button[name='submitButton']");
+  await page.click("button[id='submitButton']");
   await page.waitForURL("**/page-ipv-identity-postoffice-start");
   const title = await page.title();
   expect(title).toBe(
     "Prove your identity at a Post Office with one of the following types of photo ID – GOV.UK One Login",
   );
   await page.click("input[type='radio'][value='end']");
-  await page.click("button[name='submitButton']");
+  await page.click("button[id='submitButton']");
   await page.waitForURL("**/pyi-escape");
   const titleEscape = await page.title();
   expect(titleEscape).toBe(

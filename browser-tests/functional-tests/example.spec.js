e675d8c7c51d27932a262a6d1897d9debbbb1c8d
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-09-10 17:01:22 +0100

    PYIC-5881: Add test that checks CRI and context handling

--- browser-tests/functional-tests/example.spec.js
+++ browser-tests/functional-tests/example.spec.js
@@ -30,6 +30,34 @@ test("Example welsh language", async ({ page }) => {
   );
 });
 
+// This test goes through a realistic journey. Arguably it's overkill to just check that
+// Core front handles page contexts correctly.
+test("Example context and CRI", async ({ page }) => {
+  // Start a session
+  await page.goto(getAuthoriseUrlForJourney("exampleContext"));
+
+  // Go to the DCMAW CRI
+  await page.click("input[type='radio'][value='appTriage']");
+  await page.click("button[id='submitButton']");
+
+  // When we come back from DCMAW with access_denied, core back eventually sends us to page-multiple-doc-check
+  const url = page.url();
+  expect(url).toBe("http://localhost:4601/ipv/page/page-multiple-doc-check");
+
+  // Go to the web passport CRI
+  await page.click("input[type='radio'][value='ukPassport']");
+  await page.click("button[id='submitButton']");
+
+  // When we come back from web passport with access_denied, core back eventually sends us to prove-identity-another-type-photo-id with context 'passport'
+  const url2 = page.url();
+  expect(url2).toBe("http://localhost:4601/ipv/page/prove-identity-another-type-photo-id");
+
+  const contextSpecificTextLocator = await page.getByText("Use your UK photocard driving licence");
+  const textCount = await contextSpecificTextLocator.count();
+  expect(textCount).toBe(1);
+});
+
+// Put the journey into the 'state' parameter of the request so that imposter sends it back as the IpvSessionId
 function getAuthoriseUrlForJourney(journey) {
   return "/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fexample.com&state=" + journey + "&scope=openid+phone+email4&request=FAKE_JAR&client_id=orchestrator";
 }

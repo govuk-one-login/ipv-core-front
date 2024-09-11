4628823a13b5da67e4c6f0492e74c62395cbeed9
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-09-11 15:11:50 +0100

    PYIC-5881: Improve and split tests

--- browser-tests/functional-tests/functional.spec.js
+++ browser-tests/functional-tests/functional.spec.js
@@ -1,40 +1,58 @@
 const { test, expect } = require("@playwright/test");
 
-test("Example page navigation", async ({ page }) => {
+test("Handover from orchestration", async ({ page }) => {
+  // Start a session
+  await page.goto(getAuthoriseUrlForJourney("examplePageNavigation"));
+
+  // Check that we are on the start page
+  const url = page.url();
+  expect(url).toBe("http://localhost:4601/ipv/page/page-ipv-identity-document-start");
+});
+
+test("Page navigation", async ({ page }) => {
   // Start a session
   await page.goto(getAuthoriseUrlForJourney("examplePageNavigation"));
 
   // Go to the F2F start page
   await page.click("input[type='radio'][value='end']");
   await page.click("button[id='submitButton']");
-  await page.waitForURL("**/page-ipv-identity-postoffice-start");
 
-  // Check the page heading
-  const pageHeading = await page.locator("h1").textContent();
-  expect(pageHeading).toBe(
-    "Prove your identity at a Post Office with one of the following types of photo ID",
-  );
+  // Check that we are on the post office start page
+  const url = page.url();
+  expect(url).toBe("http://localhost:4601/ipv/page/page-ipv-identity-postoffice-start");
 });
 
-test("Example welsh language", async ({ page }) => {
+test("Welsh language toggle", async ({ page }) => {
   // Start a session
   await page.goto(getAuthoriseUrlForJourney("exampleWelshLanguage"));
 
   // Click the language toggle
   await page.click('[rel="alternate"]');
 
-  // Check the page heading
+  // Check the page heading for the start page is in Welsh
   const pageHeading = await page.locator("h1").textContent();
   expect(pageHeading).toBe(
     "Dywedwch wrthym os oes gennych un o’r mathau canlynol o ID gyda llun",
   );
 });
 
-// This test goes through a realistic journey. Arguably it's overkill to just check that
-// Core front handles page contexts correctly.
-test("Example context and CRI", async ({ page }) => {
+test("Context is used to display page", async ({ page }) => {
   // Start a session
   await page.goto(getAuthoriseUrlForJourney("exampleContext"));
+
+  // Go to the DCMAW CRI
+  await page.click("input[type='radio'][value='appTriage']");
+  await page.click("button[id='submitButton']");
+
+  // Check that we use the context returned by imposter to render the page
+  const contextSpecificTextLocator = await page.getByText("Use your UK photocard driving licence");
+  const textCount = await contextSpecificTextLocator.count();
+  expect(textCount).toBe(1);
+});
+
+test("Visiting a CRI", async ({ page }) => {
+  // Start a session
+  await page.goto(getAuthoriseUrlForJourney("exampleCri"));
 
   // Go to the DCMAW CRI
   await page.click("input[type='radio'][value='appTriage']");
@@ -43,18 +61,6 @@ test("Example context and CRI", async ({ page }) => {
   // When we come back from DCMAW with access_denied, core back eventually sends us to page-multiple-doc-check
   const url = page.url();
   expect(url).toBe("http://localhost:4601/ipv/page/page-multiple-doc-check");
-
-  // Go to the web passport CRI
-  await page.click("input[type='radio'][value='ukPassport']");
-  await page.click("button[id='submitButton']");
-
-  // When we come back from web passport with access_denied, core back eventually sends us to prove-identity-another-type-photo-id with context 'passport'
-  const url2 = page.url();
-  expect(url2).toBe("http://localhost:4601/ipv/page/prove-identity-another-type-photo-id");
-
-  const contextSpecificTextLocator = await page.getByText("Use your UK photocard driving licence");
-  const textCount = await contextSpecificTextLocator.count();
-  expect(textCount).toBe(1);
 });
 
 // Put the journey into the 'state' parameter of the request so that imposter sends it back as the IpvSessionId

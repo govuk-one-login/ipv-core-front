48541307f5d679313c9af21dc9a9813a04f543b7
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-09-30 15:47:41 +0100

    PYIC-6736: Remove getJourneyOnSniffing and update sniffPhoneType

--- src/app/shared/deviceSniffingHelper.js
+++ src/app/shared/deviceSniffingHelper.js
@@ -2,26 +2,7 @@ const UAParser = require("ua-parser-js");
 const PHONE_TYPES = require("../../constants/phone-types");
 const OS_TYPES = require("../../constants/os-types");
 
-function getJourneyOnSniffing(req) {
-  const parser = new UAParser(req.headers["user-agent"]);
-  let journeyEvent = req.body.journey;
-
-  if (parser.getDevice()["type"] === "mobile") {
-    journeyEvent += "Smartphone";
-    switch (parser.getOS()["name"]) {
-      case OS_TYPES.IOS:
-        journeyEvent += "Iphone";
-        break;
-      case OS_TYPES.ANDROID:
-        journeyEvent += "Android";
-        break;
-    }
-  }
-
-  return journeyEvent;
-}
-
-function sniffPhoneType(req, fallback) {
+function sniffPhoneType(req, fallback = "") {
   const parser = new UAParser(req.headers["user-agent"]);
 
   switch (parser.getOS()["name"]) {
@@ -35,6 +16,5 @@ function sniffPhoneType(req, fallback) {
 }
 
 module.exports = {
-  getJourneyOnSniffing,
   sniffPhoneType,
 };

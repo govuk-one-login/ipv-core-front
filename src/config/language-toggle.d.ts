19ba9614663d61f12ac5c573a96486bf61a73b26
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-10-08 17:41:23 +0100

    PYIC-7555 TS express config

---
+++ src/config/language-toggle.d.ts
@@ -0,0 +1,4 @@
+declare module "@govuk-one-login/frontend-language-toggle/build/cjs/language-param-setter.cjs" {
+  const addLanguageParam: (language: string, url: string) => void;
+  export default addLanguageParam;
+}

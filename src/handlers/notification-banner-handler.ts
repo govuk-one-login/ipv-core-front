fdffd8050a414b6bf909961f68d2993cd52438ef
Shivanshu Patel <shivanshuit914@gmail.com>
2024-10-18 12:05:24 +0100

    PYIC-7574: fix linting

--- src/handlers/notification-banner-handler.ts
+++ src/handlers/notification-banner-handler.ts
@@ -3,23 +3,26 @@ import { getParameter } from "../services/parameterStoreService";
 
 const notificationBannerHandler: RequestHandler = async (req, res, next) => {
   try {
-    const data = await getParameter('notification-banner');
-    console.log(req.params.pageId, req.query.pageId, req.originalUrl)
+    const data = await getParameter("notification-banner");
     res.locals.displayBanner = false;
     if (!data) {
       return next();
     }
-  
-    if (req.originalUrl === data.pageId) {
+
+    const currentTime = Date.now();
+    if (
+      req.originalUrl === data.pageId &&
+      currentTime >= data.startTime &&
+      currentTime <= data.endTime
+    ) {
       res.locals.displayBanner = true;
       res.locals.bannerType = data.bannerType;
       res.locals.bannerMessage = data.bannerMessage;
     }
     next();
   } catch (err) {
-    console.error(err);
     next(err);
   }
-}
+};
 
 export default notificationBannerHandler;

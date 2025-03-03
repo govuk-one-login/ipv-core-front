7876ab73bd71690cd8e21c71574f442de8fcb72f
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-03-03 11:52:17 +0000

    PYIC-7911: fix build scripts to work in container

--- build-scripts/build-service-unavailable-html.sh
+++ build-scripts/build-service-unavailable-html.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Get positional arguments to script
 htmlFilePath=$1 # destination file path for html file
@@ -10,11 +10,20 @@ ts-node ./build-scripts/build-service-unavailable-html.ts $htmlFilePath
 echo "Extracting css from $cssFilePath..."
 styling=$(grep -o '.*' $cssFilePath)
 
-# Escape special characters
-escapedStyling=${styling//$'~'/\\~}
-escapedStyling=$( echo $escapedStyling | sed 's~\!~\\\\!~g' )
+# Create copy of css file
+# The css needs to stay in a file as extracting it into an env variable results in "Arguments too long" errors
+cp $cssFilePath tempStyle.css
 
-# Replace contents of style tag in html file with that passed in from grep
-sed -i '' "s~This will be replaced in the build script.~$escapedStyling~" $htmlFilePath
+# Add style tags to beginning and end of temp style file
+echo '<style>' | cat - tempStyle.css > temp && mv temp tempStyle.css
+echo '</style>' | cat tempStyle.css - > temp && mv temp tempStyle.css
+
+echo "Adding styling to $htmlFilePath..."
+# sed --inline flag isn't compatible on all OS and so to update the html file, we need to save it to a temp file and rename it
+sed -e '/This will be replaced in the build script./ {' -e 'r tempStyle.css' -e 'd' -e '}' $htmlFilePath > tmpHtml.html
+mv tmpHtml.html $htmlFilePath
+
+# delete temp style file
+rm tempStyle.css
 
 echo "Finished creating static service-unavailable page at $htmlFilePath"
--- build-scripts/build-service-unavailable-html.ts
+++ build-scripts/build-service-unavailable-html.ts
@@ -1,10 +1,11 @@
 import fs from "fs";
 import i18next from "i18next";
 import {configureNunjucks} from "../shared/config/nunjucks";
-import enJson from "../locales/en/translation.json";
-import cyJson from "../locales/cy/translation.json";
 
 const buildServiceUnavailableHtml = (outputFile: string) => {
+  const enJson = JSON.parse(fs.readFileSync("./locales/en/translation.json", 'utf-8'))
+  const cyJson = JSON.parse(fs.readFileSync("./locales/cy/translation.json", 'utf-8'))
+
   i18next.init({
     resources: {
       en: {

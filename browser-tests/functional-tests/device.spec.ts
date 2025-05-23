29967bbd55eef148865fa6497b8ce675f00b21bd
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-05-23 17:09:34 +0100

    BAU: run linter

--- browser-tests/functional-tests/device.spec.ts
+++ browser-tests/functional-tests/device.spec.ts
@@ -5,7 +5,7 @@ const HTTP_HEADER_USER_AGENT_ANDROID =
   "Mozilla/5.0 (Linux; Android 8.0.0; Nexus 5X Build/OPR6.170623.013) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.98 Mobile Safari/537.36";
 const HTTP_HEADER_USER_AGENT_IPHONE_VALID_VERSION =
   "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/14.0 Mobile/14E277 Safari/602.1";
-  const HTTP_HEADER_USER_AGENT_IPHONE_INVALID_VERSION =
+const HTTP_HEADER_USER_AGENT_IPHONE_INVALID_VERSION =
   "Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E277 Safari/602.1";
 
 const domainUrl = process.env.WEBSITE_HOST;
@@ -27,7 +27,9 @@ test.describe("iPhone tests - valid version", () => {
     expect(url).toBe(`${domainUrl}/ipv/page/pyi-triage-mobile-download-app`);
   });
 
-  test("Selecting the download link redirects the browser to the apple app store", async ({page}) => {
+  test("Selecting the download link redirects the browser to the apple app store", async ({
+    page,
+  }) => {
     // Start a session
     await page.goto(getAuthoriseUrlForJourney("strategicAppTriageIphone"));
 
@@ -38,16 +40,26 @@ test.describe("iPhone tests - valid version", () => {
 
     // On mobile download app page with "iphone" context
     // Start waiting for redirect response before clicking button
-    const responsePromise = page.waitForResponse(response => response.status() == 301);
-    await page.getByRole('button', {name: "Download GOV.UK One Login"}).click();
+    const responsePromise = page.waitForResponse(
+      (response) => response.status() == 301,
+    );
+    await page
+      .getByRole("button", { name: "Download GOV.UK One Login" })
+      .click();
     const response = await responsePromise;
 
-    expect(response.headers()['location']).toMatch(/^.*apps\.apple\.com\/gb\/app\/gov-uk-id-check.*$/);
-  })
+    expect(response.headers()["location"]).toMatch(
+      /^.*apps\.apple\.com\/gb\/app\/gov-uk-id-check.*$/,
+    );
+  });
 
-  test("Should redirect to apple store despite the user selecting android", async ({page}) => {
+  test("Should redirect to apple store despite the user selecting android", async ({
+    page,
+  }) => {
     // Start a session
-    await page.goto(getAuthoriseUrlForJourney("userSelectsDifferentDeviceIphone"));
+    await page.goto(
+      getAuthoriseUrlForJourney("userSelectsDifferentDeviceIphone"),
+    );
 
     // On the document start page, after selecting continue, we go straight to
     // pyi-triage-mobile-download-app with android context to simulate a user choosing
@@ -57,20 +69,28 @@ test.describe("iPhone tests - valid version", () => {
 
     // On mobile download app page with "iphone" context
     // Start waiting for redirect response before clicking button
-    const responsePromise = page.waitForResponse(response => response.status() == 301);
-    await page.getByRole('button', {name: "Download GOV.UK One Login"}).click();
+    const responsePromise = page.waitForResponse(
+      (response) => response.status() == 301,
+    );
+    await page
+      .getByRole("button", { name: "Download GOV.UK One Login" })
+      .click();
     const response = await responsePromise;
 
-    expect(response.headers()['location']).toMatch(/^.*apps\.apple\.com\/gb\/app\/gov-uk-id-check.*$/);
-  })
+    expect(response.headers()["location"]).toMatch(
+      /^.*apps\.apple\.com\/gb\/app\/gov-uk-id-check.*$/,
+    );
+  });
 });
 
 test.describe("Iphone tests - invalid version", () => {
-  test.use({userAgent: HTTP_HEADER_USER_AGENT_IPHONE_INVALID_VERSION});
+  test.use({ userAgent: HTTP_HEADER_USER_AGENT_IPHONE_INVALID_VERSION });
 
   test("Handling identify-device", async ({ page }) => {
     // Start a session
-    await page.goto(getAuthoriseUrlForJourney("testIdentifyDeviceIphoneInvalidVersion"));
+    await page.goto(
+      getAuthoriseUrlForJourney("testIdentifyDeviceIphoneInvalidVersion"),
+    );
 
     // Have core-back return an identify-device page response
     // Core front should convert that page to an appTriageSmartphone event which core back will then respond to with
@@ -79,9 +99,11 @@ test.describe("Iphone tests - invalid version", () => {
     await page.click("button[id='submitButton']");
 
     const url = page.url();
-    expect(url).toBe(`${domainUrl}/ipv/page/prove-identity-another-type-photo-id`);
+    expect(url).toBe(
+      `${domainUrl}/ipv/page/prove-identity-another-type-photo-id`,
+    );
   });
-})
+});
 
 test.describe("Android tests", () => {
   test.use({ userAgent: HTTP_HEADER_USER_AGENT_ANDROID });
@@ -100,7 +122,9 @@ test.describe("Android tests", () => {
     expect(url).toBe(`${domainUrl}/ipv/page/confirm-your-details`);
   });
 
-  test("Selecting the download link redirects the browser to the google play store", async ({page}) => {
+  test("Selecting the download link redirects the browser to the google play store", async ({
+    page,
+  }) => {
     // Start a session
     await page.goto(getAuthoriseUrlForJourney("strategicAppTriageAndroid"));
 
@@ -110,14 +134,22 @@ test.describe("Android tests", () => {
     await page.click("button[id='submitButton']");
 
     // On mobile download app page with "iphone" context
-    await page.getByRole('button', {name: "Download GOV.UK One Login"}).click();
+    await page
+      .getByRole("button", { name: "Download GOV.UK One Login" })
+      .click();
 
-    expect(page.url()).toMatch(/^.*play\.google\.com\/store\/apps\/details\?id=uk\.gov\.documentchecking$/);
-  })
+    expect(page.url()).toMatch(
+      /^.*play\.google\.com\/store\/apps\/details\?id=uk\.gov\.documentchecking$/,
+    );
+  });
 
-  test("Should redirect to google play store despite the user selecting iphone", async ({page}) => {
+  test("Should redirect to google play store despite the user selecting iphone", async ({
+    page,
+  }) => {
     // Start a session
-    await page.goto(getAuthoriseUrlForJourney("userSelectsDifferentDeviceAndroid"));
+    await page.goto(
+      getAuthoriseUrlForJourney("userSelectsDifferentDeviceAndroid"),
+    );
 
     // On the document start page, after selecting continue, we go straight to
     // pyi-triage-mobile-download-app with iphone context to simulate a user choosing
@@ -126,8 +158,12 @@ test.describe("Android tests", () => {
     await page.click("button[id='submitButton']");
 
     // On mobile download app page with "iphone" context
-    await page.getByRole('button', {name: "Download GOV.UK One Login"}).click();
+    await page
+      .getByRole("button", { name: "Download GOV.UK One Login" })
+      .click();
 
-    expect(page.url()).toMatch(/^.*play\.google\.com\/store\/apps\/details\?id=uk\.gov\.documentchecking$/);
-  })
+    expect(page.url()).toMatch(
+      /^.*play\.google\.com\/store\/apps\/details\?id=uk\.gov\.documentchecking$/,
+    );
+  });
 });
--- browser-tests/functional-tests/helpers.ts
+++ browser-tests/functional-tests/helpers.ts
@@ -1,4 +1,6 @@
 // Put the journey into the 'state' parameter of the request so that imposter sends it back as the IpvSessionId
 // See the readme and/or `imposter/config/api-config.yaml` for more information
 export const getAuthoriseUrlForJourney = (journey: string): string =>
-  "/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fexample.com&state=" + journey + "&scope=openid+phone+email4&request=FAKE_JAR&client_id=orchestrator";
+  "/oauth2/authorize?response_type=code&redirect_uri=https%3A%2F%2Fexample.com&state=" +
+  journey +
+  "&scope=openid+phone+email4&request=FAKE_JAR&client_id=orchestrator";

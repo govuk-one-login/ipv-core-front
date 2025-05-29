f87301465f5931c7f1d800bda759cd5a6f2a579a
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-05-29 09:08:15 +0100

    PYIC-8330: run linting

--- src/lib/csrf.ts
+++ src/lib/csrf.ts
@@ -2,14 +2,15 @@ import { csrfSync } from "csrf-sync";
 import { Request } from "express";
 import BadRequestError from "../errors/bad-request-error";
 
-const { csrfSynchronisedProtection, getTokenFromRequest, generateToken } = csrfSync({
-  getTokenFromRequest: (req: Request) => {
-    if (!req.body) {
-      throw new BadRequestError("Missing request body");
-    }
+const { csrfSynchronisedProtection, getTokenFromRequest, generateToken } =
+  csrfSync({
+    getTokenFromRequest: (req: Request) => {
+      if (!req.body) {
+        throw new BadRequestError("Missing request body");
+      }
 
-    return req.body["_csrf"];
-  },
-});
+      return req.body["_csrf"];
+    },
+  });
 
 export { csrfSynchronisedProtection, getTokenFromRequest, generateToken };

a3cb3082cc78e54fef5dbce7b002dddb844fa211
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-09-11 16:34:06 +0100

    PYIC-8636: add f2f cri error variant

--- src/test-utils/pages-and-contexts.ts
+++ src/test-utils/pages-and-contexts.ts
@@ -65,7 +65,7 @@ export const pagesAndContexts: Record<string, (string | undefined)[]> = {
     "existingIdentityInvalid",
     undefined,
   ],
-  "sorry-technical-problem": [],
+  "sorry-technical-problem": ["f2fCriError", undefined],
   "update-details-failed": ["existingIdentityInvalid", undefined],
   "update-details": [],
   "update-name-date-birth": [

858fd4ecdd654e680c46a9ef3df9a8bba3a8fef7
Patrick Blakey <patrick.blakey@digital.cabinet-office.gov.uk>
2025-10-10 11:34:35 +0100

    INC0015498 Bump max event loop delay in line with Auth FE

--- src/app.ts
+++ src/app.ts
@@ -79,7 +79,7 @@ const protectConfig: ProtectionConfig = {
   production: process.env.NODE_ENV === "production",
   clientRetrySecs: 1,
   sampleInterval: 5,
-  maxEventLoopDelay: 400,
+  maxEventLoopDelay: 700,
   maxHeapUsedBytes: 0,
   maxRssBytes: 0,
   errorPropagationMode: true,

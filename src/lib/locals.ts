3d3e868b4f607115a635da2c225c515b960d7e77
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2026-02-02 11:19:58 +0000

    PYIC-8790: use frontend-ui progress button macro with dev timeout override

--- src/lib/locals.ts
+++ src/lib/locals.ts
@@ -16,6 +16,7 @@ export const setLocals: RequestHandler = async (req, res, next) => {
   res.locals.deleteAccountUrl = config.DELETE_ACCOUNT_URL;
   res.locals.deviceIntelligenceCookieDomain =
     config.DEVICE_INTELLIGENCE_COOKIE_DOMAIN;
+  res.locals.enableProgressButtonTimeout = config.PROGRESS_SPINNER_BUTTON_TIMEOUT;
 
   const contactUsUrl = new URL(config.CONTACT_URL);
   contactUsUrl.searchParams.set(
